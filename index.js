78fe69dec4c6c361b423993b37db0b2d38cfa8b3
nicholasgriffen <nicholas.s.griffen@gmail.com>
2019-03-12 19:21:26 -0600

    nested objects as s expressions, refactor

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 (function() {
   var registry = {
-    def: function(name) {
-      return registerSpec(name, Array.from(arguments).slice(1))
-    },
-    valid: function(spec, value) {
-      var s = spec
-      if (typeof spec === 'string') {
-        s = registry[spec]
-      }
-      for (var i = 0; i < s.predicates.length; i++) {
-        if (!s.predicates[i](value)) return null
-      }
-      if (s.toString().length && value !== stringFromSExp(s.toString())) return null
-      return true
-    }
-  }
+    getSpec: function(name) {
+    return registry[name]
+  }}
 
   function registerPredicate(specName, predicate) {
     registry[specName].predicates = registry[specName].predicates.concat(predicate)
@@ -24,30 +12,33 @@
     registry[specName] = { predicates: [] }
     registry[specName].string = parseDefArgs(specName, defArgs)
     registry[specName].toString = function() { return registry[specName].string }
-    return registry[specName]
+    return specName
   }
 
   function reduceToSExp(acc, val) {
-    if (typeof val === 'string' || typeof val === 'object') {
-      return `${acc}${atomToSExp(val)}`.replace(')(', '')
-    }
-    return acc
+    return `${acc}${toSExp(val)}`.replace(')(', '')
   }
 
-  function atomToSExp(atom) {
+  function toSExp(atom) {
     switch(typeof atom) {
       case 'string':
         return `(${atom.length}:${atom})`
       case 'object':
         if (Array.isArray(atom)) return atom.reduce(reduceToSExp, ``)
-        return `${Object.getOwnPropertyNames(atom).reduce(reduceToSExp, ``)}`
+        return objectToSExp(atom)
       default:
-        return null
+        return ''
     }
   }
 
-  function stringFromSExp(sexp) {
-    return sexp.replace(/\(\d+:(.*)\)/, '$1')
+  function objectToSExp(nested) {
+    var str = ''
+
+    for (key in nested) {
+      str = str + `(${key.length}:${key}${objectToSExp(nested[key])})`
+    }
+
+    return str.replace(/\)(\(.*)$/, '$1)')
   }
 
   function parseDefArgs(specName, defArgs) {
@@ -57,12 +48,27 @@
         return acc
       } else if (val.predicates) {
         val.predicates.forEach(predicate => registerPredicate(specName, predicate))
-        return reduceToSExp(acc, stringFromSExp(val.string))
+        return acc + toSExp(val.string)
       } else {
-        return reduceToSExp(acc, val)
+        return acc + toSExp(val)
       }
     }, ``)
   }
 
-  return registry
+  return {
+    def: function(name) {
+      return registerSpec(name, Array.from(arguments).slice(1))
+    },
+    valid: function(name, value) {
+      var spec = registry.getSpec(name)
+      for (var i = 0; i < spec.predicates.length; i++) {
+        if (!spec.predicates[i](value)) return null
+      }
+      if (spec.toString().length && toSExp(value) !== spec.toString()) return null
+      return true
+    },
+    getSpec: function(name) {
+      return registry.getSpec(name)
+    },
+  }
 })()
